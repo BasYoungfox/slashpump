b6ae7d2bdc43bf76b13a0380a69fd6479a47bbf1
Bas <bas@jongevos.com>
2026-01-08 17:50:55 +0100

    Add UtilPage component for Crypto Utility

---
+++ src/pages/app.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const UtilPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Crypto Utility</h1>
+      <p>Here’s your simple util.</p>
+    </div>
+  );
+};
+
+export default UtilPage;
